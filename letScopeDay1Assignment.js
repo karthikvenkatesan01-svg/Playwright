1da053bf56cd4d3130e884b53bd5906395e227e4
karthikvenkatesan01-svg <karthikvenkatesan01@gmail.com>
2026-06-27 08:30:20 +0530

    Add files via upload
    Day1

---
+++ letScopeDay1Assignment.js
@@ -0,0 +1,11 @@
+
+const browserName = "Chrome";
+function getBrowserNameme(){
+if(browserName === "Chrome"){
+let getBrowserName = "firefox";
+
+}
+console.log(browserName);
+}
+getBrowserName();
+
---
+++ varScopeDay1Assignment.js
@@ -0,0 +1,11 @@
+
+const browserName = "Chrome";
+function getBrowserNameme(){
+if(browserName === "Chrome"){
+var browserName = "firefox";
+
+}
+console.log(browserName);
+}
+getBrowserName();
+
