58fd0f46a88129228b9fa320089855841dde16e2
karthikvenkatesan01-svg <karthikvenkatesan01@gmail.com>
2026-06-27 13:12:57 +0530

    Add files via upload

---
+++ ScopeDay1Assignmentgender.js
@@ -0,0 +1,28 @@
+var genderType = "female";
+function printGender() {
+    let color = "brown";
+    if (genderType === "female"){
+        var age = 30;
+        let color = "Pink";
+        console.log(color);
+        
+         }
+         console.log(age);
+          console.log(color);
+}
+printGender();
+
+//2nd
+var genderType = "male"
+function printGender() {
+    let color = "brown";
+    if (genderType === "female"){
+        var age = 30;
+        let color = "Pink";
+        console.log(color);
+        
+         }
+         console.log(age);
+          console.log(color);
+}
+printGender();
